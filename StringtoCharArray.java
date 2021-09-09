0b4b203d58c155eb4f21e63a2be4ed3b90bd3f84
AZAD RANA <71632319+azadrana@users.noreply.github.com>
2021-09-09 22:01:25 +0600

    Add files via upload

---
+++ StringtoCharArray.java
@@ -0,0 +1,15 @@
+// Java program to demonstrate 
+// working of toCharArray() method 
+
+class StringtoCharArray
+ { 
+	public static void main(String args[]) 
+	{ 
+		String s = "GeeksforGeeks"; 
+		char[] gfg = s.toCharArray(); 
+		for (int i = 0; i < gfg.length; i++)
+		{ 
+			System.out.println(gfg[i]); 
+		} 
+	} 
+} 
